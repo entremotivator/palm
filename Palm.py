7c56dccccd7158411410e34a87025d5505a19426
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-10 13:02:33 -0500

    Update Palm.py

--- Palm.py
+++ Palm.py
@@ -1,13 +1,24 @@
 import streamlit as st
-import google.generativeai as palm
+import subprocess
+import json
 import os
 
-# Load environment variables from 
-
+# Load environment variables from
 
 # Retrieve PaLM API key from environment variables or st.secrets
 API_KEY = st.secrets.get("palm_api_key") or os.environ.get("PALM_API_KEY")
 palm.configure(api_key=API_KEY)
+
+def run_curl_command(prompt):
+    curl_command = [
+        "curl",
+        "-H", "Content-Type: application/json",
+        "-H", f"x-goog-api-key: {API_KEY}",
+        "-d", f'{{"prompt": {{"text": "{prompt}"}}}}',
+        "https://generativelanguage.googleapis.com/v1beta3/models/text-bison-001:generateText"
+    ]
+    result = subprocess.run(curl_command, capture_output=True, text=True)
+    return result.stdout
 
 def main():
     st.image("./Google_PaLM_Logo.svg.webp", use_column_width=False, width=100)
@@ -15,11 +26,12 @@ def main():
     st.write("")
 
     prompt = st.text_input("Prompt please...", placeholder="Prompt", label_visibility="visible")
-    temp = st.slider("Temperature", 0.0, 1.0, step=0.05)    # Hyper parameter - range[0-1]
+    temp = st.slider("Temperature", 0.0, 1.0, step=0.05)  # Hyperparameter - range[0-1]
 
     if st.button("SEND", use_container_width=True):
-        model = "models/text-bison-001"    # This is the only model currently available
+        model = "models/text-bison-001"  # This is the only model currently available
 
+        # Use PaLM API for text generation
         response = palm.generate_text(
             model=model,
             prompt=prompt,
@@ -32,6 +44,11 @@ def main():
         st.write("")
 
         st.markdown(response.result, unsafe_allow_html=False, help=None)
+
+        # Run curl command for verification
+        st.subheader("Curl Command Verification:")
+        curl_response = run_curl_command(prompt)
+        st.code(curl_response, language="json")
 
 if __name__ == "__main__":
     main()
