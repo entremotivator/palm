1b84cbe30009439cd298ae8a61b15e3a8f091255
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-10 12:41:13 -0500

    Update Palm.py

--- Palm.py
+++ Palm.py
@@ -3,11 +3,12 @@ import google.generativeai as palm
 from dotenv import load_dotenv
 import os
 
+# Load environment variables from .env file
 load_dotenv()
 
-API_KEY=os.environ.get("PALM_API_KEY")
+# Retrieve PaLM API key from environment variables or st.secrets
+API_KEY = st.secrets.get("palm_api_key") or os.environ.get("PALM_API_KEY")
 palm.configure(api_key=API_KEY)
-
 
 def main():
     st.image("./Google_PaLM_Logo.svg.webp", use_column_width=False, width=100)
@@ -15,10 +16,10 @@ def main():
     st.write("")
 
     prompt = st.text_input("Prompt please...", placeholder="Prompt", label_visibility="visible")
-    temp = st.slider("Temperature", 0.0, 1.0, step=0.05)    #Hyper parameter - range[0-1]
+    temp = st.slider("Temperature", 0.0, 1.0, step=0.05)    # Hyper parameter - range[0-1]
 
     if st.button("SEND", use_container_width=True):
-        model = "models/text-bison-001"    #This is the only model currently available
+        model = "models/text-bison-001"    # This is the only model currently available
 
         response = palm.generate_text(
             model=model,
