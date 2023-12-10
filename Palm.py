34d050a4584f6e9d5e05ef232abc9f281a470795
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-10 15:38:31 -0500

    Update Palm.py

--- Palm.py
+++ Palm.py
@@ -1,13 +1,27 @@
 import streamlit as st
 import google.generativeai as palm
 from google.api_core import retry
+import json
+
+
 
 # Constants
 API_ENDPOINT = "https://generativelanguage.googleapis.com/v1beta3/models/text-bison-001:generateText"
 HEADERS = {'Content-Type': 'application/json'}
 IMAGE_PATH = "./Google_PaLM_Logo.svg.webp"
 
-# Load environment variables from .env file
+# Load secrets from the file using st.secrets
+secrets = st.secrets["google_cloud_credentials"]
+
+# Access individual secrets
+project_id = secrets["credentials"]["project_id"]
+client_email = secrets["credentials"]["client"]["email"]
+
+# Streamlit app content
+st.title("Google Cloud Service Account Secrets Example")
+st.write(f"Project ID: {project_id}")
+st.write(f"Client Email: {client_email}")
+
 
 # Retrieve PaLM API key from environment variables or st.secrets
 API_KEY = st.secrets.get("palm_api_key")
