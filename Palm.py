e9c903641a99817c9cb01b5d42487df624a54f4a
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-10 14:47:51 -0500

    Update Palm.py

--- Palm.py
+++ Palm.py
@@ -1,28 +1,32 @@
 import streamlit as st
-import google.generativeai as palm
 import requests
 import os
+
+# Constants
+API_ENDPOINT = "https://generativelanguage.googleapis.com/v1beta3/models/text-bison-001:generateText"
+HEADERS = {'Content-Type': 'application/json'}
+IMAGE_PATH = "./Google_PaLM_Logo.svg.webp"
 
 # Load environment variables from .env file
 
 # Retrieve PaLM API key from environment variables or st.secrets
 API_KEY = st.secrets.get("palm_api_key") or os.environ.get("PALM_API_KEY")
-palm.configure(api_key=API_KEY)
 
-def generate_text_with_curl(prompt):
-    url = "https://generativelanguage.googleapis.com/v1beta3/models/text-bison-001:generateText"
-    headers = {'Content-Type': 'application/json'}
-    params = {'key': API_KEY}
-    data = {
-        "prompt": {
-            "text": prompt
-        }
-    }
-    response = requests.post(url, headers=headers, params=params, json=data)
-    return response.json()
+def configure_palm():
+    import google.generativeai as palm
+    palm.configure(api_key=API_KEY)
 
-def main():
-    st.image("./Google_PaLM_Logo.svg.webp", use_column_width=False, width=100)
+def generate_text_with_palm(prompt):
+    try:
+        response = requests.post(API_ENDPOINT, headers=HEADERS, params={'key': API_KEY}, json={"prompt": {"text": prompt}})
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        st.error(f"Error: {e}")
+        return None
+
+def display_ui():
+    st.image(IMAGE_PATH, use_column_width=False, width=100)
     st.header("Chat with PaLM")
     st.write("")
 
@@ -30,15 +34,20 @@ def main():
     temp = st.slider("Temperature", 0.0, 1.0, step=0.05)    # Hyper parameter - range[0-1]
 
     if st.button("SEND", use_container_width=True):
-        # Use the cURL-like request
-        response = generate_text_with_curl(prompt)
+        generate_and_display_response(prompt)
 
-        st.write("")
-        st.header(":blue[Response]")
-        st.write("")
+def generate_and_display_response(prompt):
+    response = generate_text_with_palm(prompt)
 
-        st.markdown(response.get("text", ""), unsafe_allow_html=False, help=None)
+    st.write("")
+    st.header(":blue[Response]")
+    st.write("")
+
+    st.markdown(response.get("text", ""), unsafe_allow_html=False, help=None)
+
+def main():
+    configure_palm()
+    display_ui()
 
 if __name__ == "__main__":
     main()
-    
