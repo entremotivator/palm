050b9bc4dd266c1bf8d75ef0632dfe6771892964
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-10 13:47:26 -0500

    Update Palm.py

--- Palm.py
+++ Palm.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import google.generativeai as palm
+import requests
 import os
 
 # Load environment variables from .env file
@@ -7,6 +8,17 @@ import os
 # Retrieve PaLM API key from environment variables or st.secrets
 API_KEY = st.secrets.get("palm_api_key") or os.environ.get("PALM_API_KEY")
 palm.configure(api_key=API_KEY)
+
+def generate_text_with_curl(prompt):
+    url = "https://generativelanguage.googleapis.com/v1beta3/models/text-bison-001:generateText?key=" + API_KEY
+    headers = {'Content-Type': 'application/json'}
+    data = {
+        "prompt": {
+            "text": prompt
+        }
+    }
+    response = requests.post(url, headers=headers, json=data)
+    return response.json()
 
 def main():
     st.image("./Google_PaLM_Logo.svg.webp", use_column_width=False, width=100)
@@ -17,20 +29,14 @@ def main():
     temp = st.slider("Temperature", 0.0, 1.0, step=0.05)    # Hyper parameter - range[0-1]
 
     if st.button("SEND", use_container_width=True):
-        model = "models/text-bison-001"    # This is the only model currently available
-
-        response = palm.generate_text(
-            model=model,
-            prompt=prompt,
-            temperature=temp,
-            max_output_tokens=1024
-        )
+        # Use the cURL-like request
+        response = generate_text_with_curl(prompt)
 
         st.write("")
         st.header(":blue[Response]")
         st.write("")
 
-        st.markdown(response.result, unsafe_allow_html=False, help=None)
+        st.markdown(response.get("text", ""), unsafe_allow_html=False, help=None)
 
 if __name__ == "__main__":
     main()
