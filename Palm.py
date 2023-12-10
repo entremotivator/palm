e0d31bcaadf0d4f7975237bf68dfb998f59445d5
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-10 16:09:49 -0500

    Update Palm.py

--- Palm.py
+++ Palm.py
@@ -12,7 +12,12 @@ configure(api_key=API_KEY)
 def retry_chat(**kwargs):
     return chat(**kwargs)
 
-def generate_and_display_response(prompt, model, context=""):
+
+def display_ui():
+    st.header("Chat with PaLM")
+    st.write("")
+
+    def generate_and_display_response(prompt, model, context=""):
     response = retry_chat(
         model=model,
         context=context,
@@ -33,28 +38,6 @@ def format_generated_text(generated_text):
     formatted_text = generated_text.capitalize()  # Example: Capitalize the text
     return formatted_text
 
-def display_ui():
-    st.header("Chat with PaLM")
-    st.write("")
-
-    prompt = st.text_input("Prompt please...", placeholder="Prompt", label_visibility="visible")
-    temp = st.slider("Temperature", 0.0, 1.0, step=0.05)  # Hyperparameter - range[0-1]
-
-    if st.button("SEND", use_container_width=True):
-        # Choose the PaLM model you want to use
-        models = [m for m in palm.list_models() if 'generateMessage' in m.supported_generation_methods]
-        if models:
-            model = models[0].name  # Adjust this based on your preference
-
-            # Optionally, provide a context for the conversation
-            context = "You are an expert at solving word problems."  # Adjust as needed
-
-            generate_and_display_response(prompt, model, context)
-        else:
-            st.error("No PaLM models found. Please check your credentials and try again.")
-
-def main():
-    display_ui()
 
 if __name__ == "__main__":
     main()
