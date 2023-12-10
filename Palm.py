762601de0f1fdcbf43d83351a80d5a8bc5cd06a3
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-10 14:59:22 -0500

    Update Palm.py

--- Palm.py
+++ Palm.py
@@ -6,32 +6,28 @@ import os
 API_ENDPOINT = "https://generativelanguage.googleapis.com/v1beta3/models/text-bison-001:generateText"
 HEADERS = {'Content-Type': 'application/json'}
 IMAGE_PATH = "./Google_PaLM_Logo.svg.webp"
-params = {'key': API_KEY}  # Use the API key obtained from Streamlit secrets
-    data = {
-        "prompt": {
-            "text": prompt
-        }
-    }
-    response = requests.post(url, headers=headers, params=params, json=data)
-    return response.json() 
 
 # Load environment variables from .env file
 
 # Retrieve PaLM API key from environment variables or st.secrets
 API_KEY = st.secrets.get("palm_api_key") 
 
-def configure_palm():
-    import google.generativeai as palm
-    palm.configure(api_key=palm_api_key)
-
 def generate_text_with_palm(prompt):
-    try:
-        response = requests.post(API_ENDPOINT, headers=HEADERS, params={'key': API_KEY}, json={"prompt": {"text": prompt}})
-        response.raise_for_status()
-        return response.json()
-    except requests.exceptions.RequestException as e:
-        st.error(f"Error: {e}")
-        return None
+    url = "https://generativelanguage.googleapis.com/v1beta3/models/text-bison-001:generateText"
+    headers = {'Content-Type': 'application/json'}
+    params = {'key': API_KEY}
+    data = {
+        "prompt": {
+            "text": prompt
+        }
+    }
+    response = requests.post(url, headers=headers, params=params, json=data)
+    return response.json()
+
+def configure_palm():
+    # Import and configuration of google.generativeai should be done here
+    import google.generativeai as palm
+    palm.configure(api_key=API_KEY)
 
 def display_ui():
     st.image(IMAGE_PATH, use_column_width=False, width=100)
@@ -39,7 +35,7 @@ def display_ui():
     st.write("")
 
     prompt = st.text_input("Prompt please...", placeholder="Prompt", label_visibility="visible")
-    temp = st.slider("Temperature", 0.0, 1.0, step=0.05)    # Hyper parameter - range[0-1]
+    temp = st.slider("Temperature", 0.0, 1.0, step=0.05)    # Hyperparameter - range[0-1]
 
     if st.button("SEND", use_container_width=True):
         generate_and_display_response(prompt)
