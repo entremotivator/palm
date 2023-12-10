f644c4d68a2c0b2c5f57ca41c66bb5ab53d81b8b
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-10 13:16:44 -0500

    Update Palm.py

--- Palm.py
+++ Palm.py
@@ -1,38 +1,35 @@
+# Author - MrSentinel
+
 import streamlit as st
-from google.generativeai import palm  # Add this line
-import subprocess
-import json
+import google.generativeai as palm
+from dotenv import load_dotenv
 import os
 
-# Retrieve PaLM API key from Streamlit secrets
-API_KEY = st.secrets.get("palm_api_key") or os.environ.get("PALM_API_KEY")
+# Load environment variables from a .env file
+load_dotenv()
+
+# Get the PaLM API key from the environment variables
+API_KEY = os.environ.get("PALM_API_KEY")
+
+# Configure PaLM with the API key
 palm.configure(api_key=API_KEY)
 
-# Store PaLM API key for curl command in Streamlit secrets
-st.secrets["curl_command_key"] = API_KEY
-
-def run_curl_command(prompt):
-    curl_command = [
-        "curl",
-        "-H", "Content-Type: application/json",
-        "-H", f"x-goog-api-key: {st.secrets['curl_command_key']}",
-        "-d", f'{{"prompt": {{"text": "{prompt}"}}}}',
-        "https://generativelanguage.googleapis.com/v1beta3/models/text-bison-001:generateText"
-    ]
-    result = subprocess.run(curl_command, capture_output=True, text=True)
-    return result.stdout
-
 def main():
+    # Access secrets using st.secrets
+    st.image("./Google_PaLM_Logo.svg.webp", use_column_width=False, width=100)
     st.header("Chat with PaLM")
     st.write("")
 
+    # User input for the prompt and temperature
     prompt = st.text_input("Prompt please...", placeholder="Prompt", label_visibility="visible")
-    temp = st.slider("Temperature", 0.0, 1.0, step=0.05)  # Hyperparameter - range[0-1]
+    temp = st.slider("Temperature", 0.0, 1.0, step=0.05)  # Hyperparameter - range [0-1]
 
+    # Button to trigger text generation
     if st.button("SEND", use_container_width=True):
-        model = "models/text-bison-001"  # This is the only model currently available
+        # Accessing secrets for PaLM model
+        model = st.secrets["palm_model"]
 
-        # Use PaLM API for text generation
+        # Generate text using the specified PaLM model
         response = palm.generate_text(
             model=model,
             prompt=prompt,
@@ -40,17 +37,11 @@ def main():
             max_output_tokens=1024
         )
 
+        # Display the generated response
         st.write("")
         st.header(":blue[Response]")
         st.write("")
-
         st.markdown(response.result, unsafe_allow_html=False, help=None)
-
-        # Run curl command for verification
-        st.subheader("Curl Command Verification:")
-        curl_response = run_curl_command(prompt)
-        st.code(curl_response, language="json")
 
 if __name__ == "__main__":
     main()
-    
