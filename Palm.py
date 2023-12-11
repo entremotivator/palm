eb5dfa909f8751fdab4cc2029bc796fc82eec691
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-11 01:14:05 -0500

    Update Palm.py

--- Palm.py
+++ Palm.py
@@ -1,37 +1,43 @@
 import streamlit as st
 import google.generativeai as palm
-from dotenv import load_dotenv
+import requests
 import os
 
-load_dotenv()
+# Load environment variables from .env file
 
-# Load API key from Streamlit secrets
-API_KEY = st.secrets["palm_api_key"]
+# Retrieve PaLM API key from environment variables or st.secrets
+API_KEY = st.secrets.get("palm_api_key") or os.environ.get("PALM_API_KEY")
 palm.configure(api_key=API_KEY)
 
-ef main():
+def generate_text_with_curl(prompt):
+    url = "https://generativelanguage.googleapis.com/v1beta3/models/text-bison-001:generateText"
+    headers = {'Content-Type': 'application/json'}
+    params = {'key': API_KEY}
+    data = {
+        "prompt": {
+            "text": prompt
+        }
+    }
+    response = requests.post(url, headers=headers, params=params, json=data)
+    return response.json()
+
+def main():
     st.image("./Google_PaLM_Logo.svg.webp", use_column_width=False, width=100)
     st.header("Chat with PaLM")
     st.write("")
 
     prompt = st.text_input("Prompt please...", placeholder="Prompt", label_visibility="visible")
-    temp = st.slider("Temperature", 0.0, 1.0, step=0.05)    #Hyper parameter - range[0-1]
+    temp = st.slider("Temperature", 0.0, 1.0, step=0.05)    # Hyper parameter - range[0-1]
 
     if st.button("SEND", use_container_width=True):
-        model = "models/text-bison-001"    #This is the only model currently available
-
-        response = palm.generate_text(
-            model=model,
-            prompt=prompt,
-            temperature=temp,
-            max_output_tokens=1024
-        )
+        # Use the cURL-like request
+        response = generate_text_with_curl(prompt)
 
         st.write("")
         st.header(":blue[Response]")
         st.write("")
 
-        st.markdown(response.result, unsafe_allow_html=False, help=None)
+        st.markdown(response.get("text", ""), unsafe_allow_html=False, help=None)
 
 if __name__ == "__main__":
     main()
